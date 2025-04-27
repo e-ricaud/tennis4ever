b97e7a3eeffe1cf27e442660492c863567d42b1d
Emile Ricaud <emile.ricaud@mirakl.com>
2025-04-27 12:11:00 +0200

    First Commit

---
+++ src/main/java/com/ericaud/tennis4ever/Tennis4everApplication.java
@@ -0,0 +1,13 @@
+package com.ericaud.tennis4ever;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tennis4everApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tennis4everApplication.class, args);
+	}
+
+}
